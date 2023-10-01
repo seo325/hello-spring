daaa4cded7747d851bfaf4b019af8ac15e81a733
seohwa0325 <seohwa0325@gmail.com>
2023-10-01 21:06:10 +0900

    spring

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,69 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+
+    }
+
+    @AfterEach
+    public void afterEach(){
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void join() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+        //when
+        Long saveId = memberService.join(member);
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+    @Test
+    public void 중복회원예외(){
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+        Member member2 = new Member();
+        member2.setName("spring");
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, ()-> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미존재하는 회원입니다.");
+//        try {
+//            memberService.join(member2);
+//            fail();
+//        }catch (IllegalAccessError e) {
+//           assertThat(e.getMessage()).isEqualTo("이미존재하는 회원입니다. ");
+//        }
+
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
