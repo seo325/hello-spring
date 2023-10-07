b7d408a26452cc123c30d5e2add254863277a7cf
seohwa0325 <seohwa0325@gmail.com>
2023-10-07 16:40:10 +0900

    orcle setting

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -4,7 +4,10 @@ import javax.persistence.*;
 
 @Entity
 public class Member {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="my_entity_seq_gen")
+    @SequenceGenerator(name="my_entity_seq_gen", sequenceName="BOARD_SEQ", initialValue=1, allocationSize=1)
     private Long id;
     @Column(name ="name")
     private String name;
